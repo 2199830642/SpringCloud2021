897ef97d1ad75097b7883112657e9ec854b0e830
Ric <2199830642@qq.com>
2021-04-07 09:05:14 +0800

    zookeeper替换eureka

---
+++ cloud-provider-payment8004zk/src/main/java/com/chen/springcloud/controller/PaymentController.java
@@ -0,0 +1,29 @@
+package com.chen.springcloud.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+/**
+ * @ClassName PaymentController
+ * @Description TODO
+ * @date 2021/4/2 16:13
+ * @Author xiaochen
+ */
+@RestController
+@Slf4j
+public class PaymentController {
+    @Value("${server.port}")
+    private String serverPort;
+
+    @RequestMapping(value = "/payment/zk")
+    public String paymentzk()
+    {
+        return "springcloud with zookeeper: "+serverPort+"\t"+ UUID.randomUUID().toString();
+    }
+
+}
