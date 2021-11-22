b8c5038b2c2ddf147ff05ab498dbcec1bebb82e0
chenkaibo <kaibo.chen@HAND-CHINA.COM>
2021-11-22 14:25:48 +0800

    增加hystrixDashBoard模块，用来监控

--- cloud-provider-hystrix-payment8001/src/main/java/com/chen/springcloud/service/impl/PaymentServiceImpl.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/chen/springcloud/service/impl/PaymentServiceImpl.java
@@ -50,7 +50,7 @@ public class PaymentServiceImpl implements PaymentService {
 
     // ============================== 服务熔断
     @HystrixCommand(fallbackMethod = "paymentCircuitBreaker_fallback",
-                    commandProperties = {@HystrixProperty(name = "circuitBreaker.enabled", value = "true"), //是否开启断路器
+                    commandProperties = {@HystrixProperty(name = "circuitBreaker.enabled", value = "true"), // 是否开启断路器
                             @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "10"), // 请求次数
                             @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "10000"), // 时间窗口期
                             @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "60")}) // 失败率达到多少就跳闸
