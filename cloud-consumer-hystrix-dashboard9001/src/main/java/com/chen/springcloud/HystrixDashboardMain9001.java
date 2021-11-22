5a59aa5c0282e460fb68a7edb6c6b7ac507924a8
chenkaibo <kaibo.chen@HAND-CHINA.COM>
2021-11-22 14:26:37 +0800

    增加hystrixDashBoard模块，用来监控

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/chen/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,19 @@
+package com.chen.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @author Kaibo.Chen@hand-china.com
+ * @ClassName: HystrixDashboardMain9001
+ * @Description: TODO
+ * @date 2021/11/22 13:48
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class, args);
+    }
+}
